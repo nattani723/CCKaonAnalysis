4e87a9217759c0d235f5be0ace3856a8c33d0c83
Natsumi Taniuchi <nat.taniuchi@gmail.com>
2024-05-10 04:39:05 -0500

    modified reco/sim particles for K analysis

--- Core/RecoParticle.h
+++ Core/RecoParticle.h
@@ -6,8 +6,6 @@
 #include "TLorentzVector.h"
 #include "TVector3.h"
 
-//#include "ubana/HyperonProduction/Headers/FV.h"
-
 #ifdef __MAKE_ROOT_DICT__
 #include "TObject.h"
 #endif
@@ -15,93 +13,120 @@
 #ifdef __MAKE_ROOT_DICT__
 class RecoParticle : public TObject{
 #else
-class RecoParticle {
+  class RecoParticle {
 #endif
 
-public:
+  public:
 
-RecoParticle(){}
-~RecoParticle(){}
+    RecoParticle(){}
+    ecoParticle(){}
 
-int Index;
+    int Index;
+    bool InNuSlice = false;
+    bool IsRebuilt = false;
 
-// General reco info
-int PDG; // Pandora PDG code (11 or 13)
-double TrackShowerScore;
-double X,Y,Z;
-double Displacement; // Distance from PV
+    // General reco info
+    int PDG; // Pandora PDG code (11 or 13)
+    int Parentage; // 1 - neutrino daughter, 2 - neutrino granddaughter, 3 - other
+    int ParentIndex=-1; // -1 - neutrino candidate or no parent
+    double TrackShowerScore;
+    double X,Y,Z;
+    double Displacement; // Distance from PV
 
-// Track variables
-double TrackLength=0;
-double TrackDirectionX=0,TrackDirectionY=0,TrackDirectionZ=0;
-double TrackStartX=0,TrackStartY=0,TrackStartZ=0;
-double TrackEndX=0,TrackEndY=0,TrackEndZ=0;
-//double TrackContained;
-double TrackPID; // 3 plane PID score
-double MeandEdX_Plane0,MeandEdX_Plane1,MeandEdX_Plane2,MeandEdX_ThreePlane; // Mean dE/dX scores
-double Track_LLR_PID; // LLR PID
-double ProtonMomentum,MuonMomentum; // Track kinematics
+    // Track variables
+    double TrackLength=0;
+    double TrackDirectionX=0,TrackDirectionY=0,TrackDirectionZ=0;
+    double TrackStartX=0,TrackStartY=0,TrackStartZ=0;
+    double TrackEndX=0,TrackEndY=0,TrackEndZ=0;
+    double TrackPID; // 3 plane PID score
+    double MeandEdX_Plane0,MeandEdX_Plane1,MeandEdX_Plane2,MeandEdX_ThreePlane; // Mean dE/dX scores
+    double Track_LLR_PID; // LLR PID
+    double Track_LLR_PID_Kaon; // LLR PID with Kaon hypothesis
+    double Track_LLR_PID_Kaon_Partial; // LLR PID with Kaon hypothesis using last 5cm of track
+    double Track_Bragg_PID_Kaon;
+    double Track_Chi2_Kaon_Plane0, Track_Chi2_Kaon_Plane1, Track_Chi2_Kaon_Plane2, Track_Chi2_Kaon_3Plane;
+    double Track_Chi2_Proton_Plane0, Track_Chi2_Proton_Plane1, Track_Chi2_Proton_Plane2, Track_Chi2_Proton_3Plane;
+    double Track_Chi2_Pion_Plane0, Track_Chi2_Pion_Plane1, Track_Chi2_Pion_Plane2, Track_Chi2_Pion_3Plane;
+    double Track_Chi2_Muon_Plane0, Track_Chi2_Muon_Plane1, Track_Chi2_Muon_Plane2, Track_Chi2_Muon_3Plane;
+    double ProtonMomentum,MuonMomentum,KaonMomentum; // Track kinematics
+    double TrackWiggliness;
 
-// Truth info
-bool HasTruth; // False if reco particle has no corresponding MC particle
-int MCTruthIndex=-1;
-int TrackTruePDG;
-double TrackTrueE,TrackTruePx,TrackTruePy,TrackTruePz;
-double TrackTrueModMomentum;
-double TrackTrueKE;
-double TrackTrueLength;
-int TrackTrueOrigin; // 1 - primary , 2 - hyperon decay, 3 - other, 4 - kaon decay, 5 - Sigma0 decay
-double TrackTruthPurity;
+    // Truth info
+    bool HasTruth; // False if reco particle has no corresponding MC particle
+    int MCTruthIndex=-1;
+    int TrackTruePDG;
+    double TrackTrueE,TrackTruePx,TrackTruePy,TrackTruePz;
+    double TrackTrueEndE,TrackTrueEndPx,TrackTrueEndPy,TrackTrueEndPz;
+    double TrackTrueModMomentum;
+    double TrackTrueEndModMomentum;
+    double TrackTrueKE;
+    double TrackTrueEndKE;
+    double TrackTrueLength;
+    int TrackTrueOrigin; // 0 = neutrino , 1 = primary , 2 = hyperon decay, 3 = other, 4 = Kaon decay, 5 = Sigma0 decay, 6 = K0S/K0L, 7 = K0S/K0L decay
+    double TrackTruthPurity;
 
-inline void SetVertex(TVector3 V);
-inline void SetTrackPositions(TVector3 Start,TVector3 End);
-inline void Print();
+    // Merge Check
+    int MergePDG_1st, MergePDG_2nd, MergePDG_3rd;
+    double MergeEnergyPurity_1st, MergeEnergyPurity_2nd, MergeEnergyPurity_3rd;
+    double MergeHitPurity_1st, MergeHitPurity_2nd, MergeHitPurity_3rd;
+
+    inline void SetVertex(TVector3 V);
+    inline void SetTrackPositions(TVector3 Start,TVector3 End);
+    inline void SetMergeCheck(std::vector<int> MergePDG, std::vector<double> MergeEnergyPurity, std::vector<double> MergeHitPurity);
+    inline void Print();
 
 #ifdef __MAKE_ROOT_DICT__
-ClassDef(RecoParticle,1);
+    ClassDef(RecoParticle,1);
 #endif
 
-};
+  };
 
 
-inline void RecoParticle::SetVertex(TVector3 V){
+  inline void RecoParticle::SetVertex(TVector3 V){
 
-X = V.X();
-Y = V.Y();
-Z = V.Z();
+    X = V.X();
+    Y = V.Y();
+    Z = V.Z();
 
-}
+  }
 
-inline void RecoParticle::SetTrackPositions(TVector3 Start,TVector3 End){
+  inline void RecoParticle::SetTrackPositions(TVector3 Start,TVector3 End){
 
-TrackStartX = Start.X();
-TrackStartY = Start.Y();
-TrackStartZ = Start.Z();
+    TrackStartX = Start.X();
+    TrackStartY = Start.Y();
+    TrackStartZ = Start.Z();
 
-TrackEndX = End.X();
-TrackEndY = End.Y();
-TrackEndZ = End.Z();
+    TrackEndX = End.X();
+    TrackEndY = End.Y();
+    TrackEndZ = End.Z();
 
+  }
 
-// Set containment
-//TrackContained = inActiveTPC(TVector3(TrackEndX,TrackEndY,TrackEndZ));
+  inline void RecoParticle::SetMergeCheck(std::vector<int> MergePDG, std::vector<double> MergeEnergyPurity, std::vector<double> MergeHitPurity){
+   
+    MergePDG_1st = MergePDG.at(0);
+    MergePDG_2nd = MergePDG.at(1);
+    MergePDG_3rd = MergePDG.at(2);
 
-}
+    MergeEnergyPurity_1st = MergeEnergyPurity.at(0);
+    MergeEnergyPurity_2nd = MergeEnergyPurity.at(1);
+    MergeEnergyPurity_3rd = MergeEnergyPurity.at(2);
 
+    MergeHitPurity_1st = MergeHitPurity.at(0);
+    MergeHitPurity_2nd = MergeHitPurity.at(1);
+    MergeHitPurity_3rd = MergeHitPurity.at(2);
+   
+  }
 
+  inline void RecoParticle::Print(){
 
-inline void RecoParticle::Print(){
+    std::cout << "Reco Info:" << std::endl;
+    std::cout << "PDG Code: " << PDG << "  Track/Shower score: " << TrackShowerScore << std::endl;
+    std::cout << "Track length: " << TrackLength << "  PID score: " << TrackPID <<  std::endl;
+    std::cout << "Truth Info:" << std::endl;
+    std::cout << "PDG: " << TrackTruePDG << "  Origin: " << TrackTrueOrigin << std::endl;
+    std::cout << "Length: " << TrackTrueLength << "  KE: " << TrackTrueKE << std::endl;
 
-std::cout << "Reco Info:" << std::endl;
-std::cout << "PDG Code: " << PDG << "  Track/Shower score: " << TrackShowerScore << std::endl;
-std::cout << "Track length: " << TrackLength << "  PID score: " << TrackPID <<  std::endl;
-std::cout << "Truth Info:" << std::endl;
-std::cout << "PDG: " << TrackTruePDG << "  Origin: " << TrackTrueOrigin << std::endl;
-std::cout << "Length: " << TrackTrueLength << "  KE: " << TrackTrueKE << std::endl;
-
-}
-
-
-
+  }
 
 #endif
--- Core/SimParticle.h
+++ Core/SimParticle.h
@@ -12,73 +12,81 @@
 #ifdef __MAKE_ROOT_DICT__
 class SimParticle : public TObject{
 #else
-class SimParticle {
+  class SimParticle {
 #endif
 
-public:
+  public:
 
-SimParticle() {}
-~SimParticle() {}
+    SimParticle() {}
+    ~SimParticle() {}
 
-int MCTruthIndex=-1; // which MCTruth this particle is the child of
+    int MCTruthIndex=-1; // which MCTruth this particle is the child of
 
-int PDG=0;
-double E=0,Px=0,Py=0,Pz=0;
-double ModMomentum=0;
-double KE=0;
-double StartX=0,StartY=0,StartZ=0;
-double EndX=0,EndY=0,EndZ=0;
-double Travel=0; // Distance travelled
-double Theta=0;
-double Phi=0;
-int Origin=0; //0 = neutrino , 1 = primary , 2 = hyperon decay, 3 = other, 4 = Kaon decay, 5 = Sigma0 decay
+    int PDG=0;
+    double E=0,Px=0,Py=0,Pz=0;
+    double ModMomentum=0;
+    double EndE=0,EndPx=0,EndPy=0,EndPz=0;
+    double EndModMomentum=0;
+    double KE=0;
+    double EndKE=0;
+    double StartX=0,StartY=0,StartZ=0;
+    double EndX=0,EndY=0,EndZ=0;
+    double Travel=0; // Distance travelled
+    double Theta=0;
+    double Phi=0;
+    int Origin=0; // 0 = neutrino , 1 = primary , 2 = Kaon+ decay, 3 = Kaon- decay, 4 = Kaon0 decay, 5 = K0S/K0L, 6 = hyperon decay, 7 = K+ -> Nu + Mu+ decay, 8 = K+ -> Pi+ + Pi0 decay, 9 = K0SL decay
 
-inline void SetKinematics(TLorentzVector P, double Mass);
-inline void SetPositions(TLorentzVector Start, TLorentzVector End);
-inline void Print();
+    inline void SetKinematics(TLorentzVector P,TLorentzVector EndP,double Mass);
+    inline void SetPositions(TLorentzVector Start,TLorentzVector End);
+    inline void Print();
 
 #ifdef __MAKE_ROOT_DICT__
-ClassDef(SimParticle,1);
+    ClassDef(SimParticle,1);
 #endif
 
-};
+  };
 
-inline void SimParticle::SetKinematics(TLorentzVector P, double Mass){
+  inline void SimParticle::SetKinematics(TLorentzVector P,TLorentzVector EndP,double Mass){
 
-//Momentum = P;
-E = P.E();
-Px = P.X();
-Py = P.Y();
-Pz = P.Z();
-ModMomentum = sqrt(Px*Px + Py*Py + Pz*Pz);
-KE = E - Mass;
+    E = P.E();
+    Px = P.X();
+    Py = P.Y();
+    Pz = P.Z();
+    ModMomentum = sqrt(Px*Px + Py*Py + Pz*Pz);
+    KE = E - Mass;
 
-Theta = (180/3.1416)*TMath::ACos(Pz/ModMomentum);
-Phi = (180/3.1416)*TMath::ASin(Py/sqrt(Px*Px+Py*Py));
+    Theta = (180/3.1416)*TMath::ACos(Pz/ModMomentum);
+    Phi = (180/3.1416)*TMath::ASin(Py/sqrt(Px*Px+Py*Py));
 
-}
+    EndE = EndP.E();
+    EndPx = EndP.X();
+    EndPy = EndP.Y();
+    EndPz = EndP.Z();
+    EndModMomentum = sqrt(EndPx*EndPx + EndPy*EndPy + EndPz*EndPz);
+    EndKE = EndE - Mass;
+  }
 
-inline void SimParticle::SetPositions(TLorentzVector Start, TLorentzVector End){ 
+  inline void SimParticle::SetPositions(TLorentzVector Start, TLorentzVector End){ 
 
-//Vertex = {Start.X() , Start.Y() , Start.Z()};	
-StartX = Start.X();
-StartY = Start.Y();
-StartZ = Start.Z();
+    //Vertex = {Start.X() , Start.Y() , Start.Z()};
+    StartX = Start.X();
+    StartY = Start.Y();
+    StartZ = Start.Z();
 
-EndX = End.X();
-EndY = End.Y();
-EndZ = End.Z();
+    EndX = End.X();
+    EndY = End.Y();
+    EndZ = End.Z();
 
-Travel = sqrt( (StartX-EndX)*(StartX-EndX) + (StartY-EndY)*(StartY-EndY) + (StartZ-EndZ)*(StartZ-EndZ) );
+    Travel = sqrt( (StartX-EndX)*(StartX-EndX) + (StartY-EndY)*(StartY-EndY) + (StartZ-EndZ)*(StartZ-EndZ) );
 
 
-}
+  }
 
-inline void SimParticle::Print(){
+  inline void SimParticle::Print(){
 
-std::cout << "PDG: " << PDG << "  Origin: " << Origin << std::endl;
-std::cout << "Length: " << Travel << "  KE: " << KE << std::endl;
+    std::cout << "PDG: " << PDG << "  Origin: " << Origin << std::endl;
+    std::cout << "Length: " << Travel << "  KE: " << KE << std::endl;
 
-}
+  }
 
 #endif
