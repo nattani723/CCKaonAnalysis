a0c33d1b1b6422fea83296415d0ec9214ad90873
Natsumi Taniuchi <165813827+nattani723@users.noreply.github.com>
2024-05-19 15:25:37 +0100

    Update EventType.h
    add PDG category

--- Core/EventType.h
+++ Core/EventType.h
@@ -18,6 +18,10 @@ const std::vector<int> Colors = {8,46,38,30,15,0};
  const std::vector<std::string> Captions3 = {"Signal","Other Kaon","Cosmic","Dirt","CCQEL","CCRES","CCDIS","CCMEC","CCCOH","NC","Other"/*,"Data"*/};
  const std::vector<int> Colors3 = {8,46,2,3,4,5,6,7,9,11,12,30,15,0};
 
+const std::vector<std::string> PDGs = {"KaonP", "Proton", "MuonP", "MuonM", "PionP", "PionM", "Shower", "Other"};
+const std::vector<std::string> CaptionsPDG = {"CC K^{+}", "proton", "#mu^{+}", "#mu^{-}", "#pi^{+}", "#pi^{-}", "Shower", "Other"};
+
+
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
 // Get the event category 
@@ -98,47 +102,22 @@ std::string GetProc(const Event &e){
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
-// Get the event category using different labelling convention for a specific MCTruth
-/*
-std::string GetType2ForTruth(const Event &e,int tr){
+// Get the PDG category 
 
-   if(!e.Mode.size()) throw std::invalid_argument("GetType2: Event has Mode.size() = 0, should be at least 1");
+std::string GetPDG(const RecoParticle &p){
 
-   if(tr > e.NMCTruths-1) 
-      throw std::invalid_argument("GetType2: Trying to get type for truth " + std::to_string(tr) + " in event with " + std::to_string(e.NMCTruths));
+   if(!p.TrackTruePDG.size()) throw std::invalid_argument("GetType: RecoParticle has TrackTruePDG.size() = 0, should be 1");
 
-   std::string type;
+ if(p.TrackTruePDG == 321) return "KaonP";
+ else if(p.TrackTruePDG == 2212) return "Proton";
+ else if(p.TrackTruePDG == -13) return "MuonP";
+ else if(p.TrackTruePDG == 13) return "MuonP";
+ else if(p.TrackTruePDG == 211) return "PionP";
+ else if(p.TrackTruePDG == -211) return "PionM";
+ else if(p.TrackTruePDG == 11 || p.TrackTruePDG == -11 || p.TrackTruePDG == 22 || p.TrackTruePDG == 111 ) return "Shower";
+ else return "Other"
 
-   if(e.Mode.at(tr) == "Data") return "Data";
-   else if(e.Mode.at(tr) == "EXT") return "EXT";
-   else if(e.Mode.at(tr) == "Dirt") return "Dirt";
-
-   // TODO: Try changing this to only look at the MC truth being inspected
-   
-   //bool islambdacharged = e.IsLambdaCharged.at(tr);
-   
-   if(e.EventIsSignal) return "DirectLambda";
-   else if(e.Mode.at(tr) == "HYP") return "DirectHYP"; 
-   //else if(e.Mode.at(tr) == "RES" && e.IsLambdaCharged.at(tr)) return "RESLambda";
-   else if(e.Mode.at(tr) == "RES" && e.IsHyperon.at(tr)) return "RESHYP"; 
-   //else if(e.Mode.at(tr) == "DIS" && e.IsLambdaCharged.at(tr)) return "DISLambda";
-   else if(e.Mode.at(tr) == "DIS" && e.IsHyperon.at(tr)) return "DISHYP"; 
-   else if(e.EventHasKaonPScatter) return "Neutron";
-   else return "Other";
-
-   bool hasKP = std::find(e.IsKaonP.begin(), e.IsKaonP.begin().end(), true) != e.IsKaonP.begin().end();
-
-   if(e.CCNC.at(0) == "CC" && e.EventIsSignal) return "CCKP";
-   else if(e.CCNC.at(0) == "NC" && e.EventIsSignal) return "NCKP";
-   else if(e.CCNC.at(0) == "CC" && hasKP && !e.inActiveTPC) return "CCKPOutFV";//not applying phasespace requirement
-   else if(e.Mode.at(0) == "KAON") return "OtherK";
-   else if(e.EventHasProtonScatter) return "Proton";
-   else return "Other";
-
-
-   return type;
 }
-*/
 
 };
 
