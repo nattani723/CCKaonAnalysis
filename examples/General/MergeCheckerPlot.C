4dd52342a5e7a57350f1998059aa8a2b56d74399
Natsumi Taniuchi <nat.taniuchi@gmail.com>
2024-07-05 05:45:30 -0500

    add weights to plot associated and single K productions at the same time

--- examples/General/MergeCheckerPlot.C
+++ examples/General/MergeCheckerPlot.C
@@ -1,15 +1,25 @@
 R__LOAD_LIBRARY($HYP_TOP/lib/libHyperon.so)
 R__LOAD_LIBRARY($HYP_TOP/lib/libParticleDict.so)
 
+
 #include "SelectionManager.h"
 #include "EventAssembler.h"
 #include "Cut.h"
 #include "SelectionParameters.h"
-
 #include "Parameters.h"
 #include "SampleSets_Example.h"
 
+// Function prototypes
+int PDGToBin(int pdg);
+std::string PDGLabel(int bin);
+void InitializeHistograms(std::map<std::string, TH1D*>& histograms);
+void AnalyzeIndividualTrack(const TVector3& PrimaryTrackEnd, const RecoParticle& DaughterTrack, TH1D* histogram, double maxDistance, int& nentry);
+void AnalyzeTracks(const Event& e, std::map<std::string, TH1D*>& histograms, int* nentry);
+void PlotHistograms(const std::map<std::string, TH1D*>& histograms);
+void HighlightBins(TH1D* hist, int bin, int color);
+void MergeCheckerPlot();
 
+// PDG to bin conversion
 int PDGToBin(int pdg) {
   switch (pdg) {
   case 321: return 1;  // K+
@@ -18,11 +28,12 @@ int PDGToBin(int pdg) {
   case 2212: return 4; // proton
   case 11:  // e-
   case -11: // e+
-  case 22:  return 5; // gamma (shower)
+  case 22: return 5; // gamma (shower)
   default: return 6;  // Others
   }
 }
 
+// Bin to PDG label conversion
 std::string PDGLabel(int bin) {
   switch (bin) {
   case 1: return "K^{+}";
@@ -38,9 +49,11 @@ std::string PDGLabel(int bin) {
 // Initialize histograms in a map
 void InitializeHistograms(std::map<std::string, TH1D*>& histograms) {
   std::vector<std::string> types = {"daughter", "rebdaughter", "shower"};
-  for (const auto& type : types) {
-    std::string name = "h_" + type;
-    std::string title = ";" + type + " Track PDG; a.u.";
+  std::vector<std::string> labels = {"Daughter Tracks", "Rebuild Daughter Tracks", "Showers"};
+  //for (const auto& type : types) {
+  for(int i=0; i<types.size(); i++){
+    std::string name = "h_" + types[i];
+    std::string title = ";Truth Match PDG of " + labels[i] + "; Energy Deposition [%]";
     histograms[name] = new TH1D(name.c_str(), title.c_str(), 6, 0, 6);
     histograms[name]->SetStats(0);
     for (int bin = 1; bin <= 6; ++bin) {
@@ -49,40 +62,29 @@ void InitializeHistograms(std::map<std::string, TH1D*>& histograms) {
   }
 }
 
+// Analyze individual track
 void AnalyzeIndividualTrack(const TVector3& PrimaryTrackEnd, const RecoParticle& DaughterTrack, TH1D* histogram, double maxDistance, int& nentry) {
-  
+  if (!histogram) {
+    std::cerr << "Error: Histogram pointer is null." << std::endl;
+    return;
+  }
+
   TVector3 DaughterTrackStart(DaughterTrack.X, DaughterTrack.Y, DaughterTrack.Z);
   if ((PrimaryTrackEnd - DaughterTrackStart).Mag() > maxDistance) return;
-
-  //Fill straight forward
-  /*
-  int bin1 = PDGToBin(DaughterTrack.MergePDG_1st) - 1;
-  if(DaughterTrack.MergePDG_1st!=-1) histogram->Fill(bin1);
-  
-  if (DaughterTrack.MergeEnergyPurity_2nd > 0.1) {
-    int bin2 = PDGToBin(DaughterTrack.MergePDG_2nd) - 1;
-    if(DaughterTrack.MergePDG_2nd!=-1) histogram->Fill(bin2);
-  }
-
-  if (DaughterTrack.MergeEnergyPurity_3rd > 0.1) {
-    int bin3 = PDGToBin(DaughterTrack.MergePDG_3rd) - 1;
-    if(DaughterTrack.MergePDG_3rd!=-1) histogram->Fill(bin3);
-  }
-  */
 
   ++nentry;
 
   int bin1 = PDGToBin(DaughterTrack.MergePDG_1st) - 1;
-  if(DaughterTrack.MergePDG_1st!=-1) histogram->Fill(bin1, DaughterTrack.MergeEnergyPurity_1st);
+  if (DaughterTrack.MergePDG_1st != -1) histogram->Fill(bin1, DaughterTrack.MergeEnergyPurity_1st);
 
   int bin2 = PDGToBin(DaughterTrack.MergePDG_2nd) - 1;
-  if(DaughterTrack.MergePDG_2nd!=-1) histogram->Fill(bin2, DaughterTrack.MergeEnergyPurity_2nd);
+  if (DaughterTrack.MergePDG_2nd != -1) histogram->Fill(bin2, DaughterTrack.MergeEnergyPurity_2nd);
 
   int bin3 = PDGToBin(DaughterTrack.MergePDG_3rd) - 1;
-  if(DaughterTrack.MergePDG_3rd!=-1) histogram->Fill(bin3, DaughterTrack.MergeEnergyPurity_3rd);
-  
+  if (DaughterTrack.MergePDG_3rd != -1) histogram->Fill(bin3, DaughterTrack.MergeEnergyPurity_3rd);
 }
 
+// Analyze tracks
 void AnalyzeTracks(const Event& e, std::map<std::string, TH1D*>& histograms, int* nentry) {
   for (size_t i_tr = 0; i_tr < e.TrackPrimaryDaughters.size(); i_tr++) {
     RecoParticle PrimaryTrack = e.TrackPrimaryDaughters[i_tr];
@@ -104,16 +106,27 @@ void AnalyzeTracks(const Event& e, std::map<std::string, TH1D*>& histograms, int
       AnalyzeIndividualTrack(PrimaryTrackEnd, e.TrackRebuiltOthers[i_tr_dau], histograms["h_rebdaughter"], 10, nentry[2]);
     }
   }
-
-  int i=0;
-  for(auto& pair : histograms){
-    TH1D* hist = pair.second;
-    hist->Scale(100/nentry[i]);
-    i++;
-  }
-
 }
 
+// Function to highlight specific bins in a histogram
+void HighlightBins(TH1D* hist, int bin, int color) {
+  if (!hist) {
+    std::cerr << "Error: Histogram pointer is null." << std::endl;
+    return;
+  }
+
+  double xlow = hist->GetXaxis()->GetBinLowEdge(bin);
+  double xhigh = hist->GetXaxis()->GetBinUpEdge(bin);
+  //double ylow = hist->GetMinimum();
+  double ylow = 0;
+  double yhigh = hist->GetBinContent(bin);
+
+  TBox* box = new TBox(xlow, ylow, xhigh, yhigh);
+  box->SetFillColorAlpha(color,0.5);
+  box->Draw("same");
+}
+
+// Plot histograms
 void PlotHistograms(const std::map<std::string, TH1D*>& histograms) {
   TCanvas* c = new TCanvas("c", "Canvas for Drawing Histograms", 800, 600);
   c->cd();
@@ -124,6 +137,8 @@ void PlotHistograms(const std::map<std::string, TH1D*>& histograms) {
 
     // Set histogram visual styles
     h->SetStats(0);  // Turn off the statistics box
+    h->SetLineColor(kBlack);
+    h->SetLineWidth(3);
     h->GetXaxis()->SetTitleFont(62);
     h->GetYaxis()->SetTitleFont(62);
     h->GetXaxis()->SetLabelFont(62);
@@ -132,6 +147,7 @@ void PlotHistograms(const std::map<std::string, TH1D*>& histograms) {
     h->GetYaxis()->SetLabelSize(0.04);
     h->GetXaxis()->SetTitleOffset(1.0);
     h->GetYaxis()->SetTitleOffset(1.5);
+    h->SetMinimum(0.0);
 
     // Setting custom bin labels if required
     h->GetXaxis()->SetBinLabel(1, "K^{+}");
@@ -144,12 +160,19 @@ void PlotHistograms(const std::map<std::string, TH1D*>& histograms) {
     // Draw the histogram
     h->Draw("HIST");
 
+    // Highlight specific bins
+    HighlightBins(h, 2, TColor::GetColorDark(kCyan));  // Highlight "mu+" bin
+    HighlightBins(h, 3, TColor::GetColorDark(kGreen));  // Highlight "pi+" bin
+    if (entry.first == "h_shower") HighlightBins(h, 5, kMagenta);  // Highlight "shower" bin
+
     // Update the canvas to reflect changes
     c->Modified();
     c->Update();
 
     // Save the canvas as a PDF or an image file
-    std::string filename = "Plots/" + entry.first + ".pdf";
+    //std::string filename = "Plots/CCK_" + entry.first + "_NuMuP.pdf";
+    //std::string filename = "Plots/CCK_" + entry.first + "_PiPPi0.pdf";
+    std::string filename = "Plots/CCK_" + entry.first + "_BothDCY.pdf";
     c->Print(filename.c_str());
 
     // Optionally clear the canvas for the next histogram
@@ -160,84 +183,82 @@ void PlotHistograms(const std::map<std::string, TH1D*>& histograms) {
   delete c;
 }
 
-
-void PlotHistograms(std::map<std::string, TH1D*>& histograms) {
-  TCanvas* c1 = new TCanvas("c1", "Canvas for Drawing Histograms", 800, 600);
-  for (const auto& hist : histograms) {
-    c1->cd();
-    hist.second->Draw();
-    c1->Update();
-    std::string filename = "Plots/" + hist.first + ".pdf";
-    c1->Print(filename.c_str());
-  }
-  delete c1;
-}
-
-void MergeCheckerPlot(){
-  
+// Main function
+void MergeCheckerPlot() {
   double POT = 1.0e21; // POT to scale samples to
   std::map<std::string, TH1D*> histograms;
-  int nentry[3] = {0,0,0};
+  int nentry[3] = {0, 0, 0};
 
   BuildTunes();
   SelectionParameters P = P_FHC_K_NOBDT_TEST;
-  
-  SampleNames.push_back("AssocKaon"); 
-  SampleTypes.push_back("AssocKaon"); 
-  SampleFiles.push_back("/exp/uboone/data/users/taniuchi/taniuchi/pandora_alg/ana/assok_refined_KrecoAlg_parameter8_ntuple.root");
+
+  SampleNames.push_back("AsoscKaon");
+  SampleTypes.push_back("AssocKaon");
+  SampleFiles.push_back("/exp/uboone/data/users/taniuchi/taniuchi/pandora_alg/ana/assok_refined_KrecoAlg_parameter10_ntuple.root");
+
+  SampleNames.push_back("SingleKaon");
+  SampleTypes.push_back("SingleKaon");
+  SampleFiles.push_back("/exp/uboone/data/users/taniuchi/taniuchi/pandora_alg/ana/singlek_refined_KrecoAlg_parameter10_ntuple.root");
+  //SampleFiles.push_back("/exp/uboone/data/users/taniuchi/taniuchi/pandora_alg/ana/cck_refined_KrecoAlg_parameter10_ntuple.root");
   //SampleFiles.push_back("/exp/uboone/data/users/taniuchi/test/KaonTrees.root");
   //SampleFiles.push_back("/exp/uboone/data/users/taniuchi/ntuple_testarea/assok_KaonTrees.root");
-  
+
   EventAssembler E;
   SelectionManager M(P);
   M.SetPOT(POT);
 
   InitializeHistograms(histograms);
-  
+
   // Sample Loop
-  for(size_t i_s=0;i_s<SampleNames.size();i_s++){
-    
-    E.SetFile(SampleFiles.at(i_s), "KAON");//2nd parameter is dummy
-    
-    if( SampleTypes.at(i_s) == "AssocKaon") M.AddSample("AssocKaon","AssocKaon",POT);
+  for (size_t i_s = 0; i_s < SampleNames.size(); i_s++) {
+    E.SetFile(SampleFiles.at(i_s), "KAON"); // 2nd parameter is dummy
+
+    if (SampleTypes.at(i_s) == "SingleKaon") M.AddSample("SingleKaon", "SingleKaon", POT);
     //if(SampleTypes.at(i_s) != "EXT" && SampleTypes.at(i_s) != "Data") M.AddSample(SampleNames.at(i_s),SampleTypes.at(i_s),E.GetPOT());
-    else if(SampleTypes.at(i_s) == "Data") M.AddSample(SampleNames.at(i_s),SampleTypes.at(i_s),Data_POT);
-    else if(SampleTypes.at(i_s) == "EXT") M.AddSample(SampleNames.at(i_s),SampleTypes.at(i_s),EXT_POT);
-    
+    else if (SampleTypes.at(i_s) == "Data") M.AddSample(SampleNames.at(i_s), SampleTypes.at(i_s), Data_POT);
+    else if (SampleTypes.at(i_s) == "EXT") M.AddSample(SampleNames.at(i_s), SampleTypes.at(i_s), EXT_POT);
+
     M.UseFluxWeight(false);
     M.UseGenWeight(false);
-    
-    // Event Loop
-    for(int i=0;i<E.GetNEvents();i++){
 
-      if(i % 10000 == 0) std::cout << i << "/" << E.GetNEvents() << std::endl;
-      
+    // Event Loop
+    for (int i = 0; i < E.GetNEvents(); i++) {
+      if (i % 10000 == 0) std::cout << i << "/" << E.GetNEvents() << std::endl;
+
       Event e = E.GetEvent(i);
-      
+
       M.SetSignal(e);
       M.AddEvent(e);
 
       //if(!e.EventIsSignal_NuMuP) continue;
-      if(!e.EventIsSignal_PiPPi0) continue;
-      
-      if(!M.FiducialVolumeCut(e)) continue;
-      if(!M.NuCCInclusiveFilter(e)) continue;
-      //if(!M.DaughterTrackCut(e)) continue;
+      //if (!e.EventIsSignal_PiPPi0) continue;
+      if (!e.EventIsSignal_NuMuP && !e.EventIsSignal_PiPPi0) continue;
+
+      if (!M.FiducialVolumeCut(e)) continue;
+      if (!M.NuCCInclusiveFilter(e)) continue;
+      if (!M.DaughterTrackCut(e)) continue;
       //if(!M.DaughterFiducialVolumeCut(e)) continue;
       //if(!M.DaughterTrackLengthCut(e)) continue;
-      
-      if(!(e.Neutrino.size() > 0 && e.Lepton.size() > 0)) continue;
-      if(e.PrimaryKaonP.size()<=0) continue;
-      if(e.EventIsSignal==false) continue;
+
+      if (!(e.Neutrino.size() > 0 && e.Lepton.size() > 0)) continue;
+      if (e.PrimaryKaonP.size() <= 0) continue;
+      if (e.EventIsSignal == false) continue;
 
       AnalyzeTracks(e, histograms, nentry);
-      
     }
     E.Close();
   }
 
+  int i = 0;
+  for (auto& pair : histograms) {
+    TH1D* hist = pair.second;
+    std::cout << "nentry[i]: " << nentry[i] << std::endl;
+    if (nentry[i] > 0) { // Avoid division by zero
+      //hist->Scale(100.0 / nentry[i]);
+      hist->Scale(100.0 / hist->Integral(0,5));
+    }
+    i++;
+  }
+
   PlotHistograms(histograms);
-
 }
-
-
